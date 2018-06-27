70b3bed3b1f25c0f37a74b982a4b28b1bbba542d
Dave Arnold <darnold@hashicorp.com>
2018-06-26 18:38:45 -0700

    fixing shit

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ variable "service_name" {}
 // Modules
 module "network" {
   source       = "app.terraform.io/Darnold-Hashicorp/network/azurerm"
-  version      = "1.0.13"
+  version      = "1.0.14"
   service_name = "${var.service_name}"
 }
