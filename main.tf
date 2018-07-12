c675e89afc41ec187d0c38369e52d3c59ab480ad
Dave Arnold <darnold@hashicorp.com>
2018-07-11 22:11:19 -0700

    bump

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ variable "env" {}
 // Modules
 module "network" {
   source  = "app.terraform.io/Darnold-Hashicorp/network/azurerm"
-  version = "1.3.0"
+  version = "1.4.0"
   env     = "${var.env}"
 }
