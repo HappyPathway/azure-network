f494caa7c4719b004a4243e648e4766278e824e4
Dave Arnold <darnold@hashicorp.com>
2018-10-01 11:54:42 -0700

    adding backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Darnold-Hashicorp"
+
+    workspaces {
+      name = "AzureSimpleAppNetwork-Dev"
+    }
+  }
+}
+
